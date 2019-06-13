f2977079e4d427652af7e6fd62840b17d401d91a
j.r.morissetdeperdigo <j.r.morissetdeperdigo@students.uu.nl>
2019-06-13 14:15:34 +0200

    credits scene and terrain magic on scene 3

---
+++ Assets/moveTerrainCredits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTerrainCredits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+    }
+}
