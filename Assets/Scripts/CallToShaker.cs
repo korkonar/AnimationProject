02dda0639ee26245d8e5f0cab901dc2cb2db2631
mnolp <38663993+mnolp@users.noreply.github.com>
2019-06-16 19:06:18 +0200

    Updated script

--- Assets/Scripts/CallToShaker.cs
+++ Assets/Scripts/CallToShaker.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CallToShaker : MonoBehaviour
 {
-    
-    public CameraShaker cameraShaker;
+
+    /// public CameraShaker cameraShaker;
+    public GameObject toActivate;
     public GameObject trigger;
     public float duration, magnitude; 
     // Update is called once per frame
     void Update()
     {
-        if (trigger.activeSelf) { 
-            StartCoroutine(cameraShaker.Shake(0.15f, 0.3f));
+        if (trigger.activeSelf) {
+            ///StartCoroutine(cameraShaker.Shake(0.15f, 0.3f));
+            toActivate.GetComponent<SimpleCameraShake>().enabled = true;
+        } else {
+            toActivate.GetComponent<SimpleCameraShake>().enabled = false;
         }
     }
 }
